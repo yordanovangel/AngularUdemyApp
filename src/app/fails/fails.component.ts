3f8d8ce4638a66b7d1cd30b84b16c6a928a83abe
Angel Yordanov <AYordanov@lirex.com>
2017-04-27 15:15:41 +0300

    init

---
+++ src/app/fails/fails.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+@Component({
+    selector:'app-fails',
+    templateUrl:'./fails.component.html',
+    styleUrls:['./fails.component.css']
+})
+
+export class FailsComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+allowNewServer = false;
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    },5000)
+  }
+
+  ngOnInit() {
+  }
+
+}
